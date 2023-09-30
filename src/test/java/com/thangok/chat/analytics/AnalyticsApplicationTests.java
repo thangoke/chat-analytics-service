a7b6d4cbad44871c034d7be854304849553f0f6d
thangok <thangokenguyen@gmail.com>
2023-09-30 16:20:08 +0700

    init chat analytics service

---
+++ src/test/java/com/thangok/chat/analytics/AnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thangok.chat.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
