20e47120f83719a508cf8370af09d5b12a194515
thangok <thangokenguyen@gmail.com>
2023-10-02 03:46:57 +0700

    implement ConversationRepository Test

---
+++ src/test/java/com/thangok/chat/analytics/ConversationRepositoryTests.java
@@ -0,0 +1,54 @@
+package com.thangok.chat.analytics;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.thangok.chat.analytics.entity.Conversation;
+import com.thangok.chat.analytics.entity.Member;
+import com.thangok.chat.analytics.repository.ConversationRepository;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversationRepositoryTests {
+  @Autowired
+  private ConversationRepository conversationRepository;
+
+  @Test
+  void contextLoads() {}
+
+  @Test
+  public void givenMemberRepository_whenSaveAndRetreiveEntity_thenOk() {
+
+    Conversation conversation = new Conversation();
+    conversation.setId("c1");
+    conversation.setName("conversation-thangok");
+    conversation.setType("INDIVIDUAL");
+
+    Member member = new Member();
+    member.setName("thangok");
+    member.setPhoneNumber("84769375847");
+    member.setConversation(conversation);
+
+    conversation.setMembers(List.of(member));
+
+    conversationRepository.save(conversation);
+
+    Conversation retrieveConversation = member.getConversation();
+    assertNotNull(retrieveConversation);
+    assertEquals(conversation.getId(), retrieveConversation.getId());
+    assertEquals(conversation.getName(), retrieveConversation.getName());
+    assertEquals(conversation.getType(), retrieveConversation.getType());
+    assertFalse(conversation.getMembers().isEmpty());
+
+    Member retrieveMember = retrieveConversation.getMembers().get(0);
+    assertNotNull(retrieveMember);
+    assertEquals(member.getId(), retrieveMember.getId());
+    assertEquals(member.getName(), retrieveMember.getName());
+    assertEquals(member.getPhoneNumber(), retrieveMember.getPhoneNumber());
+
+  }
+}
