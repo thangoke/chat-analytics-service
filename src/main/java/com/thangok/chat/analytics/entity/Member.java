a06c69195c9336cd63003b30d640b25aed540c12
thangok <thangokenguyen@gmail.com>
2023-10-01 13:40:15 +0700

    sync Member ID with Phone numer

--- src/main/java/com/thangok/chat/analytics/entity/Member.java
+++ src/main/java/com/thangok/chat/analytics/entity/Member.java
@@ -4,6 +4,9 @@ package com.thangok.chat.analytics.entity;
  * Member in Conversation.
  */
 public class Member {
+
+  private String id;
+
   private String phoneNumber;
 
   private String name;
@@ -12,8 +15,24 @@ public class Member {
     return phoneNumber;
   }
 
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  /**
+   * invoke setPhoneNumber will also set id, as assumption.
+   *
+   * @param phoneNumber phone number.
+   */
   public void setPhoneNumber(String phoneNumber) {
     this.phoneNumber = phoneNumber;
+
+    // sync entity id with phone number, as assumption.
+    this.id = phoneNumber;
   }
 
   public String getName() {
@@ -23,4 +42,5 @@ public class Member {
   public void setName(String name) {
     this.name = name;
   }
+
 }
