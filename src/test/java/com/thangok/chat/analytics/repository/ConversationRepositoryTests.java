0e18196ed40e9a5f487bb43bcec5a9d88c5a020e
thangok <thangokenguyen@gmail.com>
2023-10-02 04:48:23 +0700

    Fix test class name

--- src/test/java/com/thangok/chat/analytics/repository/ConversationRepositoryTests.java
+++ src/test/java/com/thangok/chat/analytics/repository/ConversationRepositoryTests.java
@@ -20,7 +20,7 @@ class ConversationRepositoryTests {
   void contextLoads() {}
 
   @Test
-  public void givenMemberRepository_whenSaveAndRetreiveEntity_thenOk() {
+  public void givenConversationRepository_whenSaveAndRetreiveEntity_thenOk() {
 
     Conversation conversation = new Conversation();
     conversation.setId("c1");
